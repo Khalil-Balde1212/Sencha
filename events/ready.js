ef9d0096aaadae4aa50a17d83e6f2586c251d653
Khalil Balde <33702527+Khalil-Balde1212@users.noreply.github.com>
2021-09-30 14:36:41 -0400

    No ban :roobooli:
    Removed the ban command
    Removed use of banCommandID

--- events/ready.js
+++ events/ready.js
@@ -6,18 +6,5 @@ module.exports = {
     async execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
         client.user.setActivity('I love you.');
-
-        const guild = client.guilds.cache.get(guildId);
-        const command = await guild.commands.fetch(banCommandId)
-
-        const permissions = [
-            {
-                id: adminRoleId,
-                type: 'ROLE',
-                permission: true
-            }
-        ]
-
-        await command.permissions.add({permissions})
     },
 };
